08d728ba30e35d21b22e62a46be8084e212eb2e6
shun <hmhmsh0122@gmail.com>
2018-06-07 23:00:15 +0900

    initial project

---
+++ ConnpassAPIStringBuilder/ConnpassAPIStringBuilder.h
@@ -0,0 +1,19 @@
+//
+//  ConnpassAPIStringBuilder.h
+//  ConnpassAPIStringBuilder
+//
+//  Created by hmhm on 2018/06/07.
+//  Copyright © 2018 hmhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConnpassAPIStringBuilder.
+FOUNDATION_EXPORT double ConnpassAPIStringBuilderVersionNumber;
+
+//! Project version string for ConnpassAPIStringBuilder.
+FOUNDATION_EXPORT const unsigned char ConnpassAPIStringBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnpassAPIStringBuilder/PublicHeader.h>
+
+
